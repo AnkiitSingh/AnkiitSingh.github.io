1f22d5d8b1365b8695ce7677328df1f9db5173cf
Ankit Kumar <ankit.kumar@mechademy.com>
2021-07-05 15:13:56 +0530

    Test: State Update

--- index.js
+++ index.js
@@ -20,19 +20,24 @@ const App = () => (
     const Home = () => {
         const [num, setNum] = useState(0)
         const [data, setData] = useState([])
+        const [pulse, setPulse] = useState(false)
 
         useEffect (() => {
             axios('https://catfact.ninja/fact')
             .then((res) => setData(res.data))
-        }, [])
+        }, [pulse])
 
         const increment = () => {
             setNum(num+1)
+        }
+        const changeFact = () => {
+            setPulse(!pulse)
         }
 
         return(
             <>
             <h3>{JSON.stringify(data)}</h3>
+            <button onClick={changeFact}>Change Fact</button>
             <button onClick={increment}>Increase</button>
             <h3>{JSON.stringify(num)}</h3>
             </>
